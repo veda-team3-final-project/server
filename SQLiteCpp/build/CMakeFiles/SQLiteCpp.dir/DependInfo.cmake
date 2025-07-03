37be32fde56da3ad7b6a3a4a28bafb3ab0245ccd
ckdmswo17 <ckdmswo17@naver.com>
2025-07-03 17:46:11 +0900

    last...

---
+++ SQLiteCpp/build/CMakeFiles/SQLiteCpp.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ckdmswo17/server/SQLiteCpp/src/Backup.cpp" "/home/ckdmswo17/server/SQLiteCpp/build/CMakeFiles/SQLiteCpp.dir/src/Backup.cpp.o"
+  "/home/ckdmswo17/server/SQLiteCpp/src/Column.cpp" "/home/ckdmswo17/server/SQLiteCpp/build/CMakeFiles/SQLiteCpp.dir/src/Column.cpp.o"
+  "/home/ckdmswo17/server/SQLiteCpp/src/Database.cpp" "/home/ckdmswo17/server/SQLiteCpp/build/CMakeFiles/SQLiteCpp.dir/src/Database.cpp.o"
+  "/home/ckdmswo17/server/SQLiteCpp/src/Exception.cpp" "/home/ckdmswo17/server/SQLiteCpp/build/CMakeFiles/SQLiteCpp.dir/src/Exception.cpp.o"
+  "/home/ckdmswo17/server/SQLiteCpp/src/Savepoint.cpp" "/home/ckdmswo17/server/SQLiteCpp/build/CMakeFiles/SQLiteCpp.dir/src/Savepoint.cpp.o"
+  "/home/ckdmswo17/server/SQLiteCpp/src/Statement.cpp" "/home/ckdmswo17/server/SQLiteCpp/build/CMakeFiles/SQLiteCpp.dir/src/Statement.cpp.o"
+  "/home/ckdmswo17/server/SQLiteCpp/src/Transaction.cpp" "/home/ckdmswo17/server/SQLiteCpp/build/CMakeFiles/SQLiteCpp.dir/src/Transaction.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SQLITE_ENABLE_COLUMN_METADATA"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../sqlite3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ckdmswo17/server/SQLiteCpp/build/sqlite3/CMakeFiles/sqlite3.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
